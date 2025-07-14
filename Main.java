fd3238366c086cc7824b6edf799edfcca2849f5c
veysalsariyev <118913454+veysalsariyev@users.noreply.github.com>
2025-07-14 14:21:23 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+            BorderPane root = loader.load();
+
+            Scene scene = new Scene(root, 900, 600);
+            primaryStage.setTitle("Student Management System");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
